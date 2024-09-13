d72479e7550ba4d3c0f4a201174a9df89ea59d90
hunter <hunter@STREETWARE>
2024-09-13 15:55:11 -0400

    add based.fullbright

---
+++ Fullbright/FullbrightCommand/Commands/FullbrightCommand.cs
@@ -0,0 +1,19 @@
+using Content.Shared.Administration;
+using Robust.Client.Graphics;
+using Robust.Shared.Console;
+using Robust.Shared.Toolshed.Commands.Generic;
+using Robust.Shared.IoC;
+namespace FullbrightCommand.Commands;
+
+[AnyCommand] 
+public class FullbrightCommand : IConsoleCommand
+{
+    public string Command => "based.fullbright";
+    public string Description => "Toggles fullbright mode";
+    public string Help => "HELP!";
+    [Dependency] private readonly ILightManager _light = default!;
+    public void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        _light.Enabled = !_light.Enabled;
+    }
+}
