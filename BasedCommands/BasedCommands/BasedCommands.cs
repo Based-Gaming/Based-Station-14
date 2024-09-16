963eca197347b5f57cc20bd9ffd18141511c9f35
hunter <hunter@STREETWARE>
2024-09-15 22:41:08 -0400

    add disable overlays

--- BasedCommands/BasedCommands/BasedCommands.cs
+++ BasedCommands/BasedCommands/BasedCommands.cs
@@ -1,4 +1,5 @@
 using HarmonyLib;
+using System.Reflection;
 
 public static class SubverterPatch
 {
---
+++ DisableOverlays/DisableOverlays.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using HarmonyLib;
+
+public static class MarseyPatch
+{
+    public static string Name = "Overlays patch";
+
+    public static string Description =
+        "Disables Draw on multiple overlays";
+
+    public static bool ignoreFields = true;
+}
+
+
+[HarmonyPatch]
+/// <summary>
+///  Disable overlays by disabling Draw. For example: https://github.com/space-wizards/space-station-14/blob/master/Content.Client/Drunk/DrunkOverlay.cs
+/// </summary>
+public static class OverlaysPatch
+{
+    private static MethodInfo GetOverlayDraw(string type)
+    {
+        return AccessTools.Method(AccessTools.TypeByName(type), "Draw");
+    }
+
+    private static IEnumerable<MethodBase> TargetMethods()
+    {
+        yield return GetOverlayDraw("Content.Client.Drunk.DrunkOverlay");
+        yield return GetOverlayDraw("Content.Client.Drugs.RainbowOverlay");
+        yield return GetOverlayDraw("Content.Client.Eye.Blinding.BlurryVisionOverlay");
+        yield return GetOverlayDraw("Content.Client.Eye.Blinding.BlindOverlay");
+    }
+
+    [HarmonyPrefix]
+    static bool Prefix() => false;
+}
