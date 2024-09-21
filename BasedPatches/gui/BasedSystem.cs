e5674cb440ebd402a905bd8c7edca83b4f5fbcf8
hunter <hunter@STREETWARE>
2024-09-21 05:47:15 -0400

    WIP: GUI Menu
    Why won't it load the button!?!?!

---
+++ BasedPatches/gui/BasedSystem.cs
@@ -0,0 +1,39 @@
+﻿using Robust.Client.Console;
+using Content.Client.Administration.Managers;
+using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
+
+namespace Content.Client.Based
+{
+    public sealed class BasedSystem : EntitySystem
+    {
+        [Dependency] private readonly IClientConsoleHost _consoleHost = default!;
+
+        public event Action? BasedEnabled;
+        public event Action? BasedDisabled;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void Shutdown()
+        {
+            base.Shutdown();
+        }
+
+        public void ShowJobs()
+        {
+            _consoleHost.ExecuteCommand("based.showjobs");
+        }
+
+        public void ToggleLight()
+        {
+            _consoleHost.ExecuteCommand("based.fullbright");
+        }
+        public void ToggleSubFloor()
+        {
+            _consoleHost.ExecuteCommand("based.subfloor");
+        }
+    }
+}
---
+++ BasedPatches/gui/BasedWindow.cs
@@ -0,0 +1,41 @@
+﻿using Robust.Client.UserInterface.CustomControls;
+using Robust.Client.UserInterface.Controls;
+using Robust.Client.UserInterface;
+
+namespace Content.Client.UserInterface.Systems.Based.Windows;
+
+public sealed partial class BasedWindow : DefaultWindow
+{
+    public event Action? OnDisposed;
+    public Button ShowJobIconsButton = new ();
+    public Button ToggleLightButton = new();
+    public Button ToggleSubfloorButton = new();
+
+    public BasedWindow()
+    {
+        //RobustXamlLoader.Load(this);
+        Title = "BA$ED Menu";
+        Resizable = false;
+
+        ShowJobIconsButton.Text = "Show Jobs";
+        ShowJobIconsButton.Name = "ShowJobIconsButton";
+        ShowJobIconsButton.Access = AccessLevel.Public;
+        ShowJobIconsButton.ToggleMode = true;
+
+        ToggleLightButton.Text = "Toggle Light";
+        ToggleLightButton.Name = "ToggleLightButton";
+        ToggleLightButton.Access = AccessLevel.Public;
+        ToggleLightButton.ToggleMode = true;
+
+        ToggleSubfloorButton.Text = "Toggle Subfloor";
+        ToggleSubfloorButton.Name = "ToggleSubfloorButton";
+        ToggleSubfloorButton.Access = AccessLevel.Public;
+        ToggleSubfloorButton.ToggleMode = true;
+    }
+    protected override void Dispose(bool disposing)
+    {
+        OnDisposed?.Invoke();
+        base.Dispose(disposing);
+        OnDisposed = null;
+    }
+}
