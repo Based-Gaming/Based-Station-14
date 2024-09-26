4b4ad059af7f7ca8b8455df0efb1f01bc50b3c61
hunter <hunter@STREETWARE>
2024-09-26 00:07:00 -0400

    publish

--- publish.sh
+++ publish.sh
@@ -8,13 +8,13 @@ fi
 
 cd "$(dirname "$0")"
 
-rm -rf "PUBLISH/BasedMarsey.Launcher_v${VER}.zip"
+rm -rf "PUBLISH/based_v${VER}.zip"
 mkdir -p "PUBLISH/Mods"
 
 dotnet publish sideload/Based.csproj -c Release --no-self-contained -r win-x64 /nologo
 
 cp "./bin/Release/net8.0/win-x64/publish/Based.dll"  "PUBLISH/Mods"
 cd "PUBLISH/"
-zip "based_v${VER}.zip" "Mods"
+zip -r "based_v${VER}.zip" "Mods"
 rm -rf Mods
 
