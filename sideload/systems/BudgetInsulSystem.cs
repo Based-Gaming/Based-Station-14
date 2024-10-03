8c93aa5986878f1ed52ea152947748702c432c21
hunter <hunter@STREETWARE>
2024-10-02 21:32:40 -0400

    add budget insulator detector

---
+++ sideload/systems/BudgetInsulSystem.cs
@@ -0,0 +1,24 @@
+﻿using Robust.Shared.Map;
+using Robust.Shared.GameObjects;
+using Content.Client.Examine;
+using Content.Client.Power.Generation.Teg;
+using Content.Shared.Electrocution;
+using Robust.Shared.Serialization.Manager.Attributes;
+using Robust.Shared.Prototypes;
+
+public sealed class BudgetInsulSystem : EntitySystem
+{
+    [ValidatePrototypeId<EntityPrototype>]
+    private const string Marker = "EffectEmpDisabled";
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<InsulatedComponent, ClientExaminedEvent>(BudgetInsulExamined);
+    }
+
+    private void BudgetInsulExamined(EntityUid uid, InsulatedComponent component, ClientExaminedEvent args)
+    {
+        if (component.Coefficient == 0.0)
+            Spawn(Marker, new EntityCoordinates(uid, 0, 0));
+    }
+}
